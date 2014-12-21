87cea05021b0b6e9bfa16bf7a115796f49eb7249
Jon Borgonia <jon@gomagames.com>
2014-12-20 17:49:21 -1000

    Class<Page> has no field manager

--- TestDemo.hx
+++ TestDemo.hx
@@ -1,5 +1,6 @@
 import utest.Assert;
 import sys.db.Connection;
+import ufront.db.*;
 
 class TestDemo
 {
@@ -10,11 +11,23 @@ class TestDemo
     this.cnx = cnx;
   }
 
+  public function setup():Void
+  {
+    var manager = Page.manager;
+    // var tableName = @:privateAccess manager.table_name;
+    // try TableCreate.create( Page.manager ) catch(e:Dynamic) {}
+  }
+  public function teardown():Void
+  {
+    // var tableName = @:privateAccess Page.manager.table_name;
+    // try cnx.request( 'DROP TABLE $tableName' ) catch(e:Dynamic) {}
+  }
+
   public function test_saving_page(){
     var new_page = new Page();
     new_page.title = "a new page";
     new_page.save();
-    
+
     Assert.notNull(new_page.id);
   }
 }
